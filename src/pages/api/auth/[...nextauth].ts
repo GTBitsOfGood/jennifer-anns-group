be77d3d60fc91d8a5e3e48b89fcef41ce0800868
athalanki3 <athalanki3@gatech.edu>
2024-01-30 10:26:42 -0500

    Nonfunctional authentication

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,6 +4,65 @@ import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
 import UserModel from "@/server/db/models/UserModel";
 import {MongoClient, ObjectId} from "mongodb";
 import { verifyUser } from "@/server/db/actions/UserAction";
-import { CredentialsProvider } from 'next-auth/providers';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import connectMongoDB from "@/server/db/mongodb";
+//Use MongoClient to connect to MongoDB database. 
+let cachedPromise: Promise<MongoClient>;
+const options = {};
+const URI = process.env.MONGODB_URI;
+if (!URI) {
+    throw new Error("MONGODB_URI should be an environment variable.");
+}
+const client = new MongoClient(URI,options);
 
+const retrievePromise = () => {
+    if (cachedPromise) {
+        return cachedPromise;
+    }else {
+        cachedPromise = client.connect()
+        return cachedPromise;
+    }
+}
 
+interface Credentials {
+    email: string;
+    password: string;
+}
+/**
+ *  @type {import("next-auth").NextAuthOptions}
+ */
+
+export const authOptions = {
+    adapter: MongoDBAdapter(retrievePromise()),
+    session: {
+        strategy: "jwt" as const,
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "Login with Email and Password",
+            credentials: {
+                email: {label: "Email", type: "text",placeholder:""},
+                password: {label: "Password",type: "password"}
+            },
+            async authorize(credentials: any): Promise<any> { //What am I doing here.
+                const {email, password} = credentials as Credentials;
+
+                if (!email || !password) {
+                    return null;
+                }
+                const response = await verifyUser(email,password);
+                if (response.status === 200) {
+                    return response.message; //Make sure this message includes user._id.
+                }else {
+                    return null;
+                }
+
+            }
+
+        })
+    ]
+}
+
+export default NextAuth(authOptions);
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,13 @@
 import styles from "@/styles/login.module.css";
 import Image from 'next/image';
 import jenn from "@/images/jenn-ann.png";
+import {useState, useEffect} from "react";
+import {useRouter} from "next/router";
+import {signIn} from "next-auth/react";
 export default function login() {
+    const [email, setEmail] = useState<string>("");
+    const [password,setPassword] = useState<string>("");
+    const router = useRouter();
     return <div className={styles.body}>
         <div className={styles.leftContent}>
 
@@ -15,14 +21,25 @@ export default function login() {
             
             <div className={styles.loginBox}>
                 Email <br></br>
-                <input className={styles.inputBox} type="text" id="email" placeholder="Email"></input>
+                <input className={styles.inputBox} type="text" id="email" value={email} onChange={(e) =>setEmail(e.target.value)} placeholder="Email"></input>
             </div>
             <div className={styles.loginBox}>
                 Password <br></br>
-                <input className={styles.inputBox} type="password" id="password" placeholder="Min 8 Characters"></input>
+                <input className={styles.inputBox} type="password" id="password" value={password} onChange={(e) =>setPassword(e.target.value)}placeholder="Min 8 Characters"></input>
             </div>
                 <a className={styles.forgotPassword} href="https://www.google.com/">Forgot Password?</a>
-                <button className={styles.logIn}>Log In</button>
+                <button className={styles.logIn} onClick={(e)=>{
+                    //Use document.getElementById("email")
+                    e.preventDefault();
+                    if (!email || !password) {
+                        console.log("Email or Password not provided");
+                        return;
+                    }
+                    console.log(email,password);
+                    signIn("credentials",{email:email,password:password,redirect:false});
+                    router.push("/");
+                    //Now use next-auth verification
+                }}>Log In</button>
                 <div className={styles.signupBox}>Dont have an account? <a href="/signup"className={styles.signup}>Sign up now</a><br></br><a href="/signup-admin"className={styles.signup}> Adminstrator?</a>
                 </div>
         </div>
