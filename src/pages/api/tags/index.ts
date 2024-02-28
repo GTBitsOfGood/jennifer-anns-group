dcc0b178f3cc04bc675b6c5b5c7708d909e91090
Katsuki Chan <katsukidotxyz@gmail.com>
2024-02-28 11:43:51 -0500

    feat: WIP themes/tags endpoints

--- src/pages/api/tags/index.ts
+++ src/pages/api/tags/index.ts
@@ -1,30 +1,45 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { tagSchema } from "@/utils/types";
 import { ITag } from "@/server/db/models/TagModel";
-import { createTag } from "@/server/db/actions/TagAction";
+import { createTag, getTagsByType } from "@/server/db/actions/TagAction";
 import { customErrorHandler } from "@/utils/exceptions";
+import { HTTP_STATUS_CODE } from "@/utils/consts";
+import { z } from "zod";
+
+export type CreateTagInput = ITag;
+export type DeleteTagInput = {
+  _id: string;
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    switch (req.method) {
-      case "POST":
-        let result;
-        //First ensure that req.body is of the proper type, via zod.
-        //This gives us runtime verification
-        const safeBody: ITag = tagSchema.parse(JSON.parse(req.body));
-        //Convert objectId's represented as strings into normal ObjectIds
-        result = await createTag(safeBody);
+  // try {
+  switch (req.method) {
+    case "GET":
+      getTagsByTypeHandler(req, res);
+    case "POST":
+      let result;
+      //First ensure that req.body is of the proper type, via zod.
+      //This gives us runtime verification
+      const safeBody: ITag = tagSchema.parse(JSON.parse(req.body));
+      //Convert objectId's represented as strings into normal ObjectIds
+      result = await createTag(safeBody);
 
-        return res.status(201).send({
-          data: { _id: result },
-        });
-    }
-    return res.status(405).send({
-      error: `Request method ${req.method} is not allowed`,
-    });
-  } catch (error) {
-    return customErrorHandler(res, error);
+      return res.status(201).send({
+        data: { _id: result },
+      });
   }
+  return res.status(405).send({
+    error: `Request method ${req.method} is not allowed`,
+  });
+  // } catch (error) {
+  //   return customErrorHandler(res, error);
+  // }
+}
+
+async function getTagsByTypeHandler(req: NextApiRequest, res: NextApiResponse) {
+  const tagsByType = await getTagsByType();
+  return res.status(HTTP_STATUS_CODE.OK).send(tagsByType);
 }
--- src/pages/api/themes/index.ts
+++ src/pages/api/themes/index.ts
@@ -1,29 +1,60 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { themeSchema } from "@/utils/types";
 import { ITheme } from "@/server/db/models/ThemeModel";
-import { createTheme } from "@/server/db/actions/ThemeAction";
-import { customErrorHandler } from "@/utils/exceptions";
+import { createTheme, getThemes } from "@/server/db/actions/ThemeAction";
+import {
+  ThemeException,
+  ThemeInvalidInputException,
+  customErrorHandler,
+} from "@/utils/exceptions";
+import { z } from "zod";
+import { HTTP_STATUS_CODE } from "@/utils/consts";
+
+const createThemeSchema = themeSchema.extend({
+  games: z.array(z.string().length(24)),
+});
+
+export type CreateThemeInput = z.infer<typeof createThemeSchema>;
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    switch (req.method) {
-      case "POST":
-        let result;
-        //First ensure that req.body is of the proper type, via zod.
-        //This gives us runtime verification
-        const safeBody: ITheme = themeSchema.parse(JSON.parse(req.body));
-        result = await createTheme(safeBody);
+  // try {
+  switch (req.method) {
+    case "GET":
+      return getThemeHandler(req, res);
+    case "POST":
+      return postThemeHandler(req, res);
+  }
+  return res.status(405).send({
+    error: `Request method ${req.method} is not allowed`,
+  });
+  // } catch (error) {
+  //   return customErrorHandler(res, error);
+  // }
+}
 
-        return res.status(201).send({
-          data: { _id: result },
-        });
+async function getThemeHandler(req: NextApiRequest, res: NextApiResponse) {
+  const themes = await getThemes();
+  return res.status(HTTP_STATUS_CODE.OK).send(themes);
+}
+
+async function postThemeHandler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const parsedBody = createThemeSchema.safeParse(JSON.parse(req.body));
+    if (!parsedBody.success) {
+      throw new ThemeInvalidInputException();
     }
-    return res.status(405).send({
-      error: `Request method ${req.method} is not allowed`,
+    const newTheme = await createTheme(parsedBody.data);
+    return res.status(HTTP_STATUS_CODE.OK).send({
+      ...newTheme,
+      _id: newTheme._id.toString(),
     });
-  } catch (error) {
-    return customErrorHandler(res, error);
+  } catch (e) {
+    if (e instanceof ThemeException) {
+      return res.status(e.code);
+    }
+    return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR);
   }
 }
