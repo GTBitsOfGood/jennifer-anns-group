0b224d4a117e00e0ee5bd3c560754ec8eb8ecd77
Lauren Ji <lyangji1011@gmail.com>
2024-03-02 00:18:19 -0500

    prettier (double quotes)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -30,7 +30,7 @@ const config = {
       fontFamily: {
         sans: ["Poppins", "sans-serif"], // Default `Poppins` font family to sans
         "open-sans-cond": ['"Open Sans Condensed"', "sans-serif"],
-        inter: ['Inter', 'sans-serif'],
+        inter: ["Inter", "sans-serif"],
       },
       keyframes: {
         "accordion-down": {
