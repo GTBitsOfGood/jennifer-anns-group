9461c3eba624c4e681c0f4548cb1268085128dbd
Helen Chen <helenchen@gatech.edu>
2024-03-03 18:49:48 -0500

    fix chakra theme styling

--- src/styles/chakraTheme.tsx
+++ src/styles/chakraTheme.tsx
@@ -1,64 +1,46 @@
 import { extendTheme } from "@chakra-ui/react";
 import { tabsAnatomy, tagAnatomy } from "@chakra-ui/anatomy";
 import { createMultiStyleConfigHelpers } from "@chakra-ui/react";
-const {
-  definePartsStyle: definePartsStyleTabs,
-  defineMultiStyleConfig: defineMultiStyleConfigTabs,
-} = createMultiStyleConfigHelpers(tabsAnatomy.keys);
-const {
-  definePartsStyle: definePartsStyleTags,
-  defineMultiStyleConfig: defineMultiStyleConfigTags,
-} = createMultiStyleConfigHelpers(tagAnatomy.keys);
 
-const tabVariant = definePartsStyleTabs({
-  tab: {
-    borderBottom: "4px solid",
-    color: "#98A2B3",
-    borderColor: "#EAECF0",
-    mb: "-4px",
-    fontWeight: "400",
-    _selected: {
-      color: "#164C96",
-      fontWeight: "600",
-      borderColor: "#164C96",
-      borderBottomWidth: "4px",
+const tabsHelper = createMultiStyleConfigHelpers(tabsAnatomy.keys);
+
+const Tabs = tabsHelper.defineMultiStyleConfig({
+  baseStyle: {
+    tab: {
+      borderBottom: "4px solid",
+      color: "#98A2B3",
+      borderColor: "#EAECF0",
       mb: "-4px",
+      fontWeight: "400",
+      _selected: {
+        color: "#164C96",
+        fontWeight: "600",
+        borderColor: "#164C96",
+        borderBottomWidth: "4px",
+        mb: "-4px",
+      },
+    },
+    tablist: {
+      borderBottomWidth: "4px",
     },
   },
-  tablist: {
-    borderBottomWidth: "4px",
+});
+
+const tagHelper = createMultiStyleConfigHelpers(tagAnatomy.keys);
+
+const Tag = tagHelper.defineMultiStyleConfig({
+  baseStyle: {
+    container: {
+      mr: "12px",
+      px: "18px",
+      py: "8px",
+      mb: "12px",
+      borderRadius: "full",
+    },
   },
 });
 
-const tabsTheme = defineMultiStyleConfigTabs({
-  variants: {
-    defaultTab: tabVariant,
-  },
-  defaultProps: {
-    variant: "defaultTab",
-  },
-});
-
-const tagVariant = definePartsStyleTags({
-  container: {
-    mr: "12px",
-    px: "18px",
-    py: "8px",
-    mb: "12px",
-    borderRadius: "full",
-  },
-});
-
-const tagsTheme = defineMultiStyleConfigTags({
-  variants: {
-    defaultTag: tagVariant,
-  },
-  defaultProps: {
-    variant: "defaultTag",
-  },
-});
-
-const theme = extendTheme({
+const chakraTheme = extendTheme({
   colors: {
     brand: {
       300: "#FDD299",
@@ -68,9 +50,9 @@ const theme = extendTheme({
     },
   },
   components: {
-    Tag: tagsTheme,
-    Tabs: tabsTheme,
+    Tag,
+    Tabs,
   },
 });
 
-export default theme;
+export default chakraTheme;
