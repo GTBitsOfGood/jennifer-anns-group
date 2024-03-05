399a021cd930441954a97c740ecf6ad814936127
Helen Chen <helenchen@gatech.edu>
2024-03-05 02:34:40 -0500

    fix profile modal issue

--- src/components/ProfileModal/ProfileModal.tsx
+++ src/components/ProfileModal/ProfileModal.tsx
@@ -24,7 +24,7 @@ export const userDataSchema = userSchema
   .extend({
     _id: idSchema,
   })
-  .omit({ hashedPassword: true });
+  .omit({ hashedPassword: true, notes: true });
 
 async function editUser(
   data: z.infer<typeof userDataSchema> | z.infer<typeof changePWSchema>,
