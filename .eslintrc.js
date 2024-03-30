5a8214269f0843130cabf06be00b48ade2f80783
Uma <uma2005anand@gmail.com>
2024-03-30 13:09:50 -0400

    feat: add gameboy data in initial script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: ["next", "next/core-web-vitals", "plugin:prettier/recommended"],
-  // Can't commit on Windows systems with CRLF unless this is uncommented
-  // rules: {
-  //   "prettier/prettier": ["error", { endOfLine: "auto" }],
-  // },
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
 };
