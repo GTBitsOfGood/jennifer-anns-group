d594892143233440146d689030629bd5ce4835a4
Lauren Ji <lyangji1011@gmail.com>
2024-03-30 11:19:12 -0400

    click whole card

--- src/components/GameGallery/GameCard.tsx
+++ src/components/GameGallery/GameCard.tsx
@@ -15,7 +15,13 @@ export default function GameCard({ game }: Props) {
   };
 
   return (
-    <Card borderRadius="8px" width="312px" height="380px">
+    <Card
+      onClick={goToGame}
+      cursor="pointer"
+      borderRadius="8px"
+      width="312px"
+      height="380px"
+    >
       <CardBody p="0px">
         <Image
           borderTopLeftRadius="8px"
@@ -26,16 +32,10 @@ export default function GameCard({ game }: Props) {
           src="https://img.freepik.com/free-vector/hand-drawn-houseplant-collection_23-2148910610.jpg"
         />
         <Stack mt="4" mr="5" ml="5" mb="5" spacing="2">
-          <p
-            onClick={goToGame}
-            className="cursor-pointer font-sans text-xl font-extrabold text-[#6D758F]"
-          >
+          <p className="font-sans text-xl font-extrabold text-[#6D758F]">
             {game.name}
           </p>
-          <p
-            onClick={goToGame}
-            className="line-clamp-4 cursor-pointer font-inter text-[#6D758F]"
-          >
+          <p className="line-clamp-4 font-inter text-[#6D758F]">
             {game.description}
           </p>
         </Stack>
