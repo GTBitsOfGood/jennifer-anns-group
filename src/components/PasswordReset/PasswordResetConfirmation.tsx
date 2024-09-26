80efaa0ddbe5e954fcace69f289d4f1e41b00b7d
Samarth Chandna <samarthchandna52@gmail.com>
2024-09-26 15:29:56 -0400

    nit

--- src/components/PasswordReset/PasswordResetConfirmation.tsx
+++ src/components/PasswordReset/PasswordResetConfirmation.tsx
@@ -6,7 +6,7 @@ function PasswordResetUpdate() {
   const router = useRouter();
 
   return (
-    <div className="relative flex w-[40%] min-w-[15em] flex-col gap-12">
+    <div className="flex w-[40%] min-w-[15em] flex-col gap-12">
       <h2 className="w-[100%] text-3xl font-bold text-blue-primary">
         Password Reset Successful!
       </h2>
--- src/components/PasswordReset/PasswordResetUpdate.tsx
+++ src/components/PasswordReset/PasswordResetUpdate.tsx
@@ -18,7 +18,7 @@ function ErrorNotification({
   onClick: () => void;
 }) {
   return (
-    <div className="absolute top-[-10em] flex w-[120%] items-center gap-2 rounded-md border-2 border-solid border-red-400 p-3">
+    <div className="absolute top-[-18vh] flex w-[20%] min-w-[20em] items-center gap-2 rounded-md border-2 border-solid border-red-400 p-3">
       <img className="h-5 w-5" src="/error.svg" alt="Error Icon" />
       <div className="flex min-w-[90%] items-start justify-between">
         <span className="text-xs">
