c0fe16fbefa582ff05a969840b6fac1281c81e11
Hayden Carpenter <haydencarp@outlook.com>
2024-11-04 21:47:41 -0500

    bad ankith

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     domains: ["cloudflare-b2.bogjenniferanns.workers.dev"],
   },
+  webpack(config, { dev, isServer }) {
+    if (!dev && !isServer) {
+      config.devtool = 'source-map';
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
